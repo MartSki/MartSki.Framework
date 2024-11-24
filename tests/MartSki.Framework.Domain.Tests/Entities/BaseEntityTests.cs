395aba2220a287d0d3ae7401be9db8d68a3e44c2
mart-ski <mskidev@gmail.com>
2024-11-24 09:12:26 +0100

    test(entities): Création test BaseEntity avec Id int

---
+++ tests/MartSki.Framework.Domain.Tests/Entities/BaseEntityTests.cs
@@ -0,0 +1,22 @@
+﻿using FluentAssertions;
+using MartSki.Framework.Domain.Tests.Entities.Models;
+
+namespace MartSki.Framework.Domain.Tests;
+
+public class BaseEntityTests
+{
+    [SetUp]
+    public void Setup()
+    {
+    }
+
+    [TestCase(1, "Bon", "Jean", 42)]
+    public void BaseIdInt_ClientAvecId(int id, string nom, string prénom, int age)
+    {
+        Client client = new(id, nom, prénom, age);
+
+        client.Id.Should().NotBe(null);
+        client.Id.GetType().Should().Be(typeof(int));
+        client.Id.Should().Be(id);
+    }
+}
