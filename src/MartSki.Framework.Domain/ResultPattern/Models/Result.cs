96435a544916240084f9574992bd01c8e9705ec9
mart-ski <mskidev@gmail.com>
2024-11-24 10:44:21 +0100

    feat(resultPattern): Implémentation Result.Success

---
+++ src/MartSki.Framework.Domain/ResultPattern/Models/Result.cs
@@ -0,0 +1,20 @@
+﻿using MartSki.Framework.Domain.ResultPattern.Interfaces;
+
+namespace MartSki.Framework.Domain.ResultPattern.Models
+{
+    public class Result : IResult
+    {
+        public bool IsSuccess { get; init; }
+        public bool IsFailure { get; init; }
+        public Error Error { get; init; }
+
+        protected Result(bool isSuccess, Error? error = null)
+        {
+            IsSuccess = isSuccess;
+            IsFailure = isFailure;
+            Error = error;
+        }
+
+        public static Result Success() => new(true);
+    }
+}
---
+++ tests/MartSki.Framework.Domain.Tests/ResultPattern/ResultTests.cs
@@ -0,0 +1,22 @@
+﻿using FluentAssertions;
+using MartSki.Framework.Domain.ResultPattern.Models;
+
+namespace MartSki.Framework.Domain.Tests;
+
+public class ResultTests
+{
+    [SetUp]
+    public void Setup()
+    {
+    }
+
+    [Test]
+    public void Success_ResultSuccess()
+    {
+        Result result = Result.Success();
+
+        result.IsSuccess.Should().BeTrue();
+        result.IsFailure.Should().BeFalse();
+        result.Error.Should().BeNull();
+    }
+}
