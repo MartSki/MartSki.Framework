f874d8e2035a5aa0d1927e706f628c4e1b400acc
mart-ski <mskidev@gmail.com>
2024-11-24 10:35:42 +0100

    feat(resultPattern): Définition interface Result

---
+++ src/MartSki.Framework.Domain/ResultPattern/Interfaces/IResult.cs
@@ -0,0 +1,16 @@
+﻿using MartSki.Framework.Domain.ResultPattern.Models;
+
+namespace MartSki.Framework.Domain.ResultPattern.Interfaces
+{
+    public interface IResult<TValue> : IResult
+    {
+        TValue Value { get; init; }
+    }
+
+    public interface IResult
+    {
+        public bool IsSuccess { get; init; }
+        public bool IsFailure { get; init; }
+        public Error Error { get; init; }
+    }
+}
