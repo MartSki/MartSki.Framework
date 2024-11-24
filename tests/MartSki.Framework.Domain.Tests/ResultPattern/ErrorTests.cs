8d33437c8df513ff6aae16be828c141a9d63df6b
mart-ski <mskidev@gmail.com>
2024-11-24 10:23:04 +0100

    test(resultPattern): Test nominal création d'une erreur

---
+++ tests/MartSki.Framework.Domain.Tests/ResultPattern/ErrorTests.cs
@@ -0,0 +1,21 @@
+﻿using FluentAssertions;
+using MartSki.Framework.Domain.ResultPattern.Models;
+
+namespace MartSki.Framework.Domain.Tests;
+
+public class ErrorTests
+{
+    [SetUp]
+    public void Setup()
+    {
+    }
+
+    [TestCase("Mon code", "Mon Message")]
+    public void Erreur_CodeEtMessageDéfinis_ErreurAvecCodeEtMessage(string code, string message)
+    {
+        Error error = new(code, message);
+
+        error.Code.Should().Be(code);
+        error.Message.Should().Be(message);
+    }
+}
